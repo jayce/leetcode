eeabbe772d9048a753782ca24b5b9a8c21a0893f
jayce <jayce.liuchuan@gmail.com>
2020-11-16 13:25:50 +0800

    update: 617.
    Signed-off-by: jayce <jayce.liuchuan@gmail.com>

--- 0617.go
+++ 0617.go
@@ -5,7 +5,7 @@
 package leetcode
 
 // mergeTrees First idea
-func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode { // time: O(N)
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode { // time: O(min(N,M))
 	if t1 == nil {
 		return t2
 	}
@@ -16,4 +16,22 @@ func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode { // time: O(N)
 	t1.Left = mergeTrees(t1.Left, t2.Left)
 	t1.Right = mergeTrees(t1.Right, t2.Right)
 	return t1
+}
+
+// Second idea time: O(max(N,M))
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	if t1 == nil && t2 == nil {
+		return nil
+	}
+	if t1 == nil {
+		t1 = &TreeNode{}
+	}
+	if t2 == nil {
+		t2 = &TreeNode{}
+	}
+	return &TreeNode{
+		Val:   t1.Val + t2.Val,
+		Left:  mergeTrees(t1.Left, t2.Left),
+		Right: mergeTrees(t1.Right, t2.Right),
+	}
 }
