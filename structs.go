3cd6456f88994e0fcc8aec24faaec92b34ea38fd
jayce <jayce.liuchuan@gmail.com>
2021-05-14 14:41:28 +0800

    chore: added some structures to autocomplete.
    Signed-off-by: jayce <jayce.liuchuan@gmail.com>

---
+++ structs.go
@@ -0,0 +1,16 @@
+/*
+ * Creation in 2021-05-13 by Chuan Liu (Jayce)
+ */
+
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
